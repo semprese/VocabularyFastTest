a0fd9114bb8b7c330d0d85c46f34dc59c0d8cfea
Mikoroso <mikorosolain@gmail.com>
2025-02-04 18:19:08 +0300

    Insert WordList Logic

--- app/src/main/java/com/bignerdranch/android/voctest/func/WordManager.kt
+++ app/src/main/java/com/bignerdranch/android/voctest/func/WordManager.kt
@@ -1,4 +1,14 @@
 package com.bignerdranch.android.voctest.func
 
 class WordManager {
+    private var currentGroup: WordGroup = WordGroup.createInitial()
+
+    fun handleCheck(word: Word, isKnown: Boolean) {
+        if (isKnown) currentGroup.distribution.markWordAsKnown(word)
+        else currentGroup.distribution.markWordAsUnknown(word)
+    }
+
+    fun moveToNextGroup() {
+        currentGroup = currentGroup.createNextGroup(180)
+    }
 }
